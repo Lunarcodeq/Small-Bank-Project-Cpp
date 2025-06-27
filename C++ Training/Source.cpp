8fb2011b63df46531a2d7e663e6e73f5e0f4644a
Lunar <imidasss7@gmail.com>
2025-06-28 00:36:29 +0300

    Add project files.

---
+++ C++ Training/Source.cpp
@@ -0,0 +1,280 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+string fileName = "Clients.txt";
+
+enum enMenues { Show = 1, Add = 2, Del = 3, Edit = 4, Find = 5, Exit = 6 };
+
+struct stClientRecord {
+
+	string AccountNumber;
+	string Pincode;
+	string FullName;
+	string PhoneNumber;
+	double Balance;
+	bool MarkToDelete = false;
+	bool MarkToEdit = false;
+
+};
+
+stClientRecord AddClient() {
+
+	stClientRecord Client;
+
+	cout << "\nEnter Account Number: \n";
+	getline(cin >> ws, Client.AccountNumber);
+
+	cout << "Enter Pincode: \n";
+	getline(cin, Client.Pincode);
+
+	cout << "Enter Name: \n";
+	getline(cin, Client.FullName);
+
+	cout << "Enter Phone Number: \n";
+	getline(cin, Client.PhoneNumber);
+
+	cout << "Enter Balance: \n";
+	cin >> Client.Balance;
+
+	return Client;
+}
+
+vector <stClientRecord> AddMoreClients() {
+
+	char answer = 'n';
+	stClientRecord ClientInfo;
+
+	vector <stClientRecord> vClientsInfo;
+
+	do {
+
+		ClientInfo = AddClient();
+
+		vClientsInfo.push_back(ClientInfo);
+
+		cout << "Do you want to add more clients? [Y/N]\n";
+		cin >> answer;
+
+	} while (toupper(answer) == 'Y');
+
+	return vClientsInfo;
+
+}
+
+string ConvertInfosToRecord(stClientRecord ClientInfo, string delim = "#//#") {
+
+	string Record = "";
+
+	Record += ClientInfo.AccountNumber + delim;
+	Record += ClientInfo.Pincode + delim;
+	Record += ClientInfo.FullName + delim;
+	Record += ClientInfo.PhoneNumber + delim;
+	Record += to_string(ClientInfo.Balance);
+
+	return Record;
+}
+
+void UploadClientsRecord(vector <stClientRecord> ClientsInfo) {
+
+	stClientRecord Records;
+	
+	fstream file;
+
+	file.open(fileName, ios::out | ios::app);
+
+	if (file.is_open()) {
+
+		string line = "";
+
+		for (stClientRecord& info : ClientsInfo) {
+
+			line = ConvertInfosToRecord(info);
+			file << line << endl;
+
+		}
+
+		file.close();
+	}
+
+
+}
+
+vector <string> SplitRecordWord(string line, string delim = "#//#") {
+
+	vector <string> vStringRecords;
+	short pos = 0;
+
+	string word = "";
+
+	while ((pos = line.find(delim)) != std::string::npos) {
+
+		word = line.substr(0, pos);
+		vStringRecords.push_back(word);
+
+		line.erase(0, pos + delim.length());
+	}
+
+
+	if (line != "") {
+		vStringRecords.push_back(line);
+	}
+
+	return vStringRecords;
+}
+
+stClientRecord LoadLinesToRecord(vector <string> splittedLine) {
+
+	stClientRecord Client;
+
+	Client.AccountNumber = splittedLine[0];
+	Client.Pincode = splittedLine[1];
+	Client.FullName = splittedLine[2];
+	Client.PhoneNumber = splittedLine[3];
+	Client.Balance = stod(splittedLine[4]);
+	
+	return Client;
+
+}
+
+vector <stClientRecord> ClientsRecord() {
+
+	vector <stClientRecord> Clients;
+	vector <string> ClientsString;
+
+	fstream file;
+
+	file.open(fileName, ios::in);
+
+	if (file.is_open()) {
+
+		string line = "";
+		stClientRecord record;
+
+		while (getline(file, line)) {
+
+			ClientsString = SplitRecordWord(line);
+			record = LoadLinesToRecord(ClientsString);
+
+			Clients.push_back(record);
+
+		}
+		
+
+		file.close();
+	}
+
+	return Clients;
+
+}
+
+void PrintRecord(stClientRecord Record) {
+
+	cout << "| " << setw(19) << left << Record.AccountNumber;
+	cout << "| " << setw(16) << left << Record.Pincode;
+	cout << "| " << setw(36) << left << Record.FullName;
+	cout << "| " << setw(19) << left << Record.PhoneNumber;
+	cout << "| " << setw(15) << left << Record.Balance;
+
+}
+
+void ShowClients() {
+
+	vector <stClientRecord> cRecords = ClientsRecord();
+
+	cout << "\t\t\t\t\tClient List (" << cRecords.size() << ") Client(s).\n";
+
+	cout << "______________________________________________________________";
+	cout << "__________________________________________________________\n\n";
+	cout << "|" << setw(20) << left << " Account Number" << "|" << setw(17) << left << " Pincode";
+	cout << "|" << setw(37) << left << " Client Name" << "|" << setw(20) << left << " Phone" << "|" << setw(16) << " Balance";
+	cout << "\n______________________________________________________________";
+	cout << "__________________________________________________________\n\n";
+
+
+	for (stClientRecord& record : cRecords) {
+
+		PrintRecord(record);
+		cout << endl;
+
+	}
+
+
+	cout << "\n______________________________________________________________";
+	cout << "__________________________________________________________\n\n";
+}
+
+void AddNewClients() {
+
+	cout << "\n---------------------------------------\n";
+	cout << "\tAdd New Clients Screen\n";
+	cout << "---------------------------------------\n";
+
+	vector <stClientRecord> ClientsInfo = AddMoreClients();
+	UploadClientsRecord(ClientsInfo);
+
+}
+
+void MainMenue() {
+
+	system("cls");
+
+	enMenues menu;
+
+	cout << "===========================================\n";
+	cout << "\t\tMain Menue Screen\n";
+	cout << "===========================================\n";
+
+	cout << "\t" << "[1] Show Clients List. \n";
+	cout << "\t" << "[2] Add New Client. \n";
+	cout << "\t" << "[3] Delete Client. \n";
+	cout << "\t" << "[4] Update Client Info. \n";
+	cout << "\t" << "[5] Find Client. \n";
+	cout << "\t" << "[6] Exit. \n";
+
+	cout << "==========================================\n";
+
+	short answer;
+	cout << "Choose what do you want to do ? [1-6]\n";
+	cin >> answer;
+
+	while (cin.fail()) {
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		cout << "Choose what do you want to do ? [1-6]\n";
+		cin >> answer;
+	}
+
+	switch (answer) {
+
+	case enMenues::Show:
+
+		system("cls");
+		ShowClients();
+		system("pause");
+		MainMenue();
+		break;
+
+	case enMenues::Add:
+
+		system("cls");
+		AddNewClients();
+		system("pause");
+		MainMenue();
+		break;
+
+	}
+
+	system("pause>0");
+}
+
+int main() {
+
+	MainMenue();
+
+	return 0;
+}
